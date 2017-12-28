85cadcf7515b47f93113b9300479866eec72c142
knite51 <knite51@gmail.com>
2017-12-28 09:53:42 +0100

    model auto generated file for models

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,9 @@
+
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.json`)[env];
@@ -10,23 +13,21 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  sequelize = new Sequelize(
-    config.database, config.username, config.password, config
-  );
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(file =& gt;
-(file.indexOf('.') !== 0) & amp;& amp;
-(file !== basename) & amp;& amp;
-(file.slice(-3) === '.js'))
-  .forEach(file =& gt; {
-  const model = sequelize.import(path.join(__dirname, file));
-  db[model.name] = model;
-});
+  .filter((file) => {
+    return (file.indexOf('.') !== 0) && (file !== basename);
+  })
+  .forEach((file) => {
+    if (file.slice(-3) !== '.js') return;
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
-Object.keys(db).forEach(modelName =& gt; {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }

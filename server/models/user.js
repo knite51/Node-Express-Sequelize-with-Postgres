6737dee70fccc5108b6f5ef43af0a459ab8e574f
knite51 <knite51@gmail.com>
2017-12-28 09:54:36 +0100

    user model construct

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: DataTypes.STRING,
+  }, {
+    classMethods: {
+      associate(models) {
+        // associations can be defined here
+        User.hasMany(models.Todo);
+      },
+    },
+  });
+  return User;
+};

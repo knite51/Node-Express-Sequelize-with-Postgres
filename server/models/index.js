583d4febe8203bc9fa3589b1c8e8bf233afa8351
knite51 <knite51@gmail.com>
2018-01-03 03:05:24 +0100

    refactor using ES6 modules import syntax

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,6 @@
-
-
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
@@ -36,4 +34,4 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 
-module.exports = (sequelize, DataTypes) => {
+export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: DataTypes.STRING,
   }, {

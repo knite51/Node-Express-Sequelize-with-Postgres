c4c11b7beeb6305f5cd485b346349ee03ee010ea
knite51 <knite51@gmail.com>
2018-01-11 19:20:01 +0100

    Refactor file and import routes

--- bin/www.js
+++ bin/www.js
@@ -1,6 +1,10 @@
 // This will be our application entry. We'll setup our server here.
 /* eslint-disable no-console */
-import app from '../app';
+import { userRoutes, todoRoutes } from '../server/routes';
+import app from './../app';
+
+app.use('/user', userRoutes);
+app.use('/todo', todoRoutes);
 
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 const msg = `Magic happening at port ${PORT}`;

b773bb3e4b6b1f880fe8d40a5263c8d432b6deba
knite51 <knite51@gmail.com>
2018-01-15 16:34:52 +0100

    authenticate user by adding token verification system

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,8 @@
-
+import jwt from 'jsonwebtoken';
 import models from '../models/';
+
+const PrivateKey = process.env.SECRET || 'passkey';
+const { User } = models;
 
 export default {
   // Post a User
@@ -10,21 +13,35 @@ export default {
       username,
       password,
     } = userInfo;
-    models.User.create({
-      email,
-      username,
-      password,
+    User.findOne({
+      where: {
+        $or: [
+          { email },
+          { username },
+        ],
+      },
     })
-      .then((user) => {
-        return res.status(200).send({ user, message: 'Successfully created User!' });
-      })
-      .catch((err) => {
-        throw new Error(err);
+      .then((userReturned) => {
+        if (userReturned) {
+          return res.status(409).send({ message: 'User Already Exists' });
+        }
+        return User.create({
+          email,
+          username,
+          password,
+        })
+          .then((user) => {
+            const token = jwt.sign({ id: user.id, username: user.username }, PrivateKey);
+            return res.status(201).send({ user, token, message: 'Successfully created User! Kindly Copy Token to login!' });
+          })
+          .catch((err) => {
+            throw new Error(err);
+          });
       });
   },
   // Get a user
-  fetchUser: (req, res) => {
-    models.User.findAll({})
+  fetchUsers: (req, res) => {
+    User.findAll({})
       .then((user) => {
         if (user.length !== 0) {
           return res.status(200).send({ user, message: 'All users fetched!' });
@@ -34,6 +51,84 @@ export default {
       .catch((err) => {
         throw new Error(err);
       });
+  },
+  updateUserDetails: (req, res) => {
+    const userPayload = req.body;
+    User.findById(req.params.id)
+      .then((user) => {
+        if (!user) {
+          res.status(404).send({ message: 'No such user exists yet!' });
+        }
+        user.update(userPayload)
+          .then((updatedUser) => {
+            res.status(200).send({ updatedUser, message: 'Update Successful!' });
+          })
+          .catch(err => res.status(501).send({ err, message: 'Update not Successful!' }));
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  },
+  deleteUser: (req, res) => {
+    User.destroy({
+      where: { id: req.params.id },
+    })
+      .then((user) => {
+        if (user === 1) {
+          return res.status(200).send({ message: 'User Removed' });
+        }
+        return res.status(404).send({ message: 'Hello Macha Such User Doesnt Exisit!' });
+      })
+      .catch((err) => {
+        res.status(501).send({ err, message: 'Delete unsuccessful!' });
+      });
+  },
+  fetchOneUser: (req, res) => {
+    User.findById(req.params.id)
+      .then((user) => {
+        if (!user) {
+          res.status(404).send({ message: 'User not found' });
+        }
+        res.status(200).send({ user, message: 'Sucessful!' });
+      })
+      .catch((err) => {
+        res.status(501).send({ err, message: 'An error occured' });
+      });
+  },
+  loginUser: (req, res) => {
+    const { identifier, password } = req.body;
+    User.find({
+      where: {
+        $or: [
+          { email: identifier },
+          { username: identifier },
+        ],
+      },
+    })
+      .then((user) => {
+        if (!user) {
+          return res.status(401).json({ message: 'Invalid credentials My friend' });
+        }
+        if (user.password !== password) {
+          return res.status(401).json({ message: 'Invalid Password Boy' });
+        }
+        const token = jwt.sign({
+          id: user.id,
+          username: user.username,
+        }, PrivateKey);
+        return res.status(200).send({
+          user: {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+          },
+          token,
+          message: 'Login Successful! Token expires in one day.',
+        });
+      });
+  },
+  logoutUser: (req, res) => {
+    res.status(200).send({ message: 'User successfully logged out!' });
   },
 };
 
